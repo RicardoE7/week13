8612fd1f3c64e8606664acdc040919a22a549784
Ricardo Edwards <ricardo.edwards4899@gmail.com>
2024-07-11 18:25:22 -0400

    Updated pom and config
    Updated spring boot annotation in application class. Switched to version 3.1.5 in pom file. Fixed application context start error and bean error.

--- src/main/java/com/coderscampus/week13/Week13Application.java
+++ src/main/java/com/coderscampus/week13/Week13Application.java
@@ -2,8 +2,12 @@ package com.coderscampus.week13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@ComponentScan("com.coderscampus.week13")
+
 public class Week13Application {
 
 	public static void main(String[] args) {
